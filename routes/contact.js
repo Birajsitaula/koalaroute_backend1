636ba1b07f01b7aad6ac8d0d2e3b5622a87b8133
Birajsitaula <birajsitaula7@gmail.com>
2025-09-02 20:03:31 +0545

    update in the contact

--- routes/contact.js
+++ routes/contact.js
@@ -1,21 +1,3 @@
-// import { Router as ExpressRouter } from "express";
-
-// const router = ExpressRouter();
-
-// router.post("/", (req, res) => {
-//   const { name, email, message } = req.body;
-//   if (!name || !email || !message)
-//     return res.status(400).json({ msg: "All fields required" });
-
-//   // Here you can send email using nodemailer or save to DB
-//   res.json({
-//     msg: "Contact form submitted successfully",
-//     data: { name, email, message },
-//   });
-// });
-
-// export default router;
-
 import express from "express";
 import nodemailer from "nodemailer";
 
