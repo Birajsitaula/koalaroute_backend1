14947dc11a0b54107a10b4c4a6822d6ca21f96cf
Birajsitaula <birajsitaula7@gmail.com>
2025-09-03 13:35:23 +0545

    update

--- app.js
+++ app.js
@@ -12,13 +12,8 @@ dotenv.config();
 const app = express();
 
 // Middleware
-// app.use(cors({ origin: "*", credentials: true }));
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://koalaroute-frontend.vercel.app"],
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*", credentials: true }));
+
 // some chages here
 app.use(express.json());
 
