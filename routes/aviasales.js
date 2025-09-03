98545918081cf7455f036e7fed59b6c516b74580
Birajsitaula <birajsitaula7@gmail.com>
2025-09-03 22:12:22 +0545

    testing ..

--- routes/aviasales.js
+++ routes/aviasales.js
@@ -238,7 +238,7 @@ const API_URL = "https://api.travelpayouts.com/v1/flight_search";
 const RESULTS_URL = "https://api.travelpayouts.com/v1/flight_search_results";
 
 const MARKER = process.env.AVIASALES_MARKER;
-const TOKEN = process.env.AVIASALES_TOKEN;
+const TOKEN = process.env.AVIASALES_API_KEY;
 const LOCALE = "en";
 
 // Logging to confirm environment variables are loaded
