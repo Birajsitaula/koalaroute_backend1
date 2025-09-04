86b34c4d7fa04ae18f7ff472b55b73eeb75315f1
Birajsitaula <birajsitaula7@gmail.com>
2025-09-04 22:29:37 +0545

    bug fixed

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.ADMIN_EMAIL, // your admin email
-    pass: process.env.ADMIN_EMAIL_PASSWORD, // app password
+    pass: process.env.ADMIN_PASSWORD, // app password
   },
 });
 
