ef22d6d6b959e0e99e415ce088f5592ecbb1eeb4
Birajsitaula <birajsitaula7@gmail.com>
2025-10-16 09:36:04 +0545

    testing

--- app.js
+++ app.js
@@ -45,3 +45,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server is live and listening on port ${PORT}`);
 });
+
+// Export app for testing or further configuration
