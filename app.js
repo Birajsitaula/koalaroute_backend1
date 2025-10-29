992493281124d76d643c9ec705ff1cf554b85170
Birajsitaula <birajsitaula7@gmail.com>
2025-10-29 21:13:56 +0545

    some small bug fixed

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/koalaroute", duffelFlights);
+// app.use("/api/koalaroute", duffelFlights);
+app.use("/api", duffelFlights);
 app.use("/api/chat", chatRouter);
 app.use("/api/contact", contactRoutes);
 // ... other routes
